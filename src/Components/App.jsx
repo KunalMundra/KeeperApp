0b8e06699d235e2836ebd2767024ee6e944bda56
Kunal Mundra <kunalmundra2@gmail.com>
2023-04-25 20:33:55 +0530

    Third Commit

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import Notes from "../notes"
-
-
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-        <Header />
-        <Footer />
-        {Notes.map(noteItem => <Note
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-        />
-        )}
+
+  const [notes, setNotes] = useState([])
+
+  function AddNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    })
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+  return (
+    <div>
+      <Header />
+
+      <CreateArea onAdd={AddNote} />
+      {notes.map((noteItem, index) => {
+        return <Note key={index} id = {index} title = {noteItem.title} content = {noteItem.content} onDelete = {deleteNote}/>
+
+      })}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Footer(){
-
-    const date = new Date();
-    const year = date.getFullYear();
-    return <footer>
-        <p>Copyright {year}</p>
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
     </footer>
+  );
 }
 
-export default Footer 
-
+export default Footer;
